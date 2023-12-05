81d56977db2f003ffbdb9f9f63e1a471b07ae0ef
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-12-05 11:49:01 +0100

    ninja

--- RandomGame/GameScene.swift
+++ RandomGame/GameScene.swift
@@ -9,101 +9,119 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    //MARK: - Properties
+    var ground: SKSpriteNode!
+    var player: SKSpriteNode!
+    var cameraNode = SKCameraNode()
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    var cameraMovePointPerSecond: CGFloat = 450.0
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var lastUpdateTime: TimeInterval = 0.0
+    var dt: TimeInterval = 0.0
     
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+    var playableRect: CGRect{
+        let ratio: CGFloat
+        switch UIScreen.main.nativeBounds.height{
+        case 2688, 1792, 2436:
+            ratio = 2.16
+        default:
+            ratio = 16/9
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        let playableHeight = size.width / ratio
+        let playableMargin = 
+        return CGRect(x: 0.0, y: playableMargin, width: size.width, height: playableHeight)
     }
     
+    //MARK: - Systems
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    override func didMove(to view: SKView) {
+        setupNodes()
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        if lastUpdateTime > 0{
+            dt = currentTime - lastUpdateTime
+            
+        } else {
+            dt = 0
         }
         
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+        lastUpdateTime = currentTime
+        moveCamera()
+        //print(dt)
     }
+    
+   
+}
+
+//MARK: - Configurations
+
+extension GameScene {
+    func setupNodes() {
+        createBG()
+        createGround()
+        createPlayer()
+        setupCamera()
+    }
+    
+    func createBG() {
+        for i in 0...2{
+            let bg = SKSpriteNode(imageNamed: "background")
+            bg.name = "BG"
+            bg.anchorPoint = .zero
+            bg.position = CGPoint(x: CGFloat(i)*bg.frame.width, y: 0.0  )
+            bg.zPosition = -1.0
+            addChild(bg)
+        }
+    }
+    
+    func createGround() {
+        for i in 0...2{
+            ground = SKSpriteNode(imageNamed: "ground")
+            ground.name = "Ground"
+            ground.anchorPoint = .zero
+            ground.zPosition = 1.0
+            ground.position = CGPoint(x: CGFloat(i)*ground.frame.width, y: 0.0)
+            addChild(ground)
+        }
+    }
+    
+    func createPlayer(){
+        player = SKSpriteNode(imageNamed: "ninja")
+        player.name = "Player"
+        player.zPosition = 5.0
+        player.position = CGPoint(x: frame.width/2.0 - 100.0, y: ground.frame.height + player.frame.height / 2.0)
+        addChild(player)
+    }
+    
+    func setupCamera(){
+        addChild(cameraNode)
+        camera = cameraNode
+        cameraNode.position = CGPoint(x: frame.midX, y: frame.midY)
+    }
+    
+    func moveCamera(){
+        let amountToMove = CGPoint(x: cameraMovePointPerSecond*CGFloat(dt), y:0.0)
+        cameraNode.position += amountToMove
+        //CGPoint(x: cameraNode.position.x + amountToMove.x, y: cameraNode.position.y + amountToMove.y)
+        
+        //Background
+        enumerateChildNodes(withName: "BG") { (node, _) in
+            let node = node as! SKSpriteNode
+            
+            if node.position.x + node.frame.width < self.cameraNode.frame.origin.x {
+                node.position = CGPoint(x: node.position.x + node.frame.width * 2.0, y: node.position.y)
+            }
+        }
+        //Ground
+        enumerateChildNodes(withName: "Ground") { (node, _) in
+            let node = node as! SKSpriteNode
+            
+            if node.position.x + node.frame.width < self.cameraNode.frame.origin.x {
+                node.position = CGPoint(x: node.position.x + node.frame.width * 2.0, y: node.position.y)
+            }
+        }
+    }
+    
 }
--- RandomGame/GameViewController.swift
+++ RandomGame/GameViewController.swift
@@ -13,40 +13,14 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let scene = GameScene(size: CGSize(width: 2048, height: 1536))
+        scene.scaleMode = .aspectFill
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
-        }
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.showsPhysics = true
+        skView.presentScene(scene)
     }
 
     override var prefersStatusBarHidden: Bool {
